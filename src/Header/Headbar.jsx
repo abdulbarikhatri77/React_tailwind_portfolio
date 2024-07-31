a27c3beb6a16c92e9b6f61495ed8b67a086901aa
abdulbarikhatri77 <ababdulbarikhatri@gmail.com>
2024-07-31 17:01:58 +0500

    add some stylings and one pdf

--- src/Header/Headbar.jsx
+++ src/Header/Headbar.jsx
@@ -1,13 +1,47 @@
+import { TypeAnimation } from "react-type-animation";
+
+
 const Navbar = () => {
   const scrollToSection = (sectionId) => {
     document.getElementById(sectionId).scrollIntoView({ behavior: "smooth" });
   };
 
+  const onButtonClick = () => {
+    const pdfUrl = "file.pdf";
+    const link = document.createElement("a");
+    link.href = pdfUrl;   
+    // link.download = "/documents/file.pdf";
+    link.download = "abc_file"
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+
+  
+
+
+  };
+
   return (
     <div className="flex justify-between bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg mx-auto p-6">
-      <div className="text-3xl text-black font-extrabold px-4 bg-green-300 p-2 rounded-lg">
-        Abdul Bari Portfolio
-      </div>
+      <TypeAnimation className="text-3xl text-black font-extrabold px-4 bg-green-300 p-2 rounded-lg"
+      sequence={[
+        'Abdul Bari khatri',
+        900,
+        'WellCome To My Portfolio',
+        900,
+        () => {
+          console.log('Sequence completed');
+        },
+      ]}
+      wrapper="span"
+      cursor={true}
+      repeat={Infinity}
+      style={{ fontSize: '2em', display: 'inline-block' }}
+    />
+
+    
+        
+      
       <nav>
         <button
           className="text-white hover:text-black font-extrabold text-2xl cursor-pointer bg-lime-500 py-2 px-4 rounded-lg mx-2"
@@ -33,6 +67,7 @@ const Navbar = () => {
         >
           Contact
         </button>
+        <button  className="text-white hover:text-black font-extrabold text-2xl cursor-pointer bg-lime-500 py-2 px-4 rounded-lg mx-2"  onClick={onButtonClick}>My Ressume</button>
       </nav>
     </div>
   );
--- src/body components/MainBody.jsx
+++ src/body components/MainBody.jsx
@@ -1,14 +1,22 @@
+import Image1 from '../assets/image1.jpeg'
+
 const MainBody = () => {
+   
   return (
-    <div>
+    
+      
+ <div>
+   <img className="bg-cover bg-center h-screen w-full" src={Image1} alt="" />
+
       <main className="container mx-auto mt-16">
         <section id="home" className="py-16">
           <h1 className="text-4xl font-bold">Home</h1>
+          <a href='/documents/file.pdf' download='/documents/file.pdf'>Download file</a>
           <p className="mt-4">This is the home section.</p>
         </section>
         <section id="about" className="py-16">
           <h1 className="text-4xl font-bold">About</h1>
-          <p className="mt-4">This is the about section.</p>
+          <h3 className="mt-4">HELLO THERE ! My name is Abdul Bari, and I am a skilled freelancer specializing <br/> in Python, JavaScript, React, React Native, and data administration. With a proven <br/> track record of delivering high-quality solutions, I bring technical expertise and <br/>creativity to every project. My focus is on exceeding client expectations and staying <br/> updated with industry trends. Lets connect and explore how I can help bring your vision to life.</h3>
         </section>
         <section id="services" className="py-16">
           <h1 className="text-4xl font-bold">Services</h1>
@@ -20,6 +28,7 @@ const MainBody = () => {
         </section>
       </main>
     </div>
+   
   );
 };
 
